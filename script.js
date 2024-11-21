05e83cdb6c2c429a8cdb4ff9a2d619c414072e26
Ahmad-U19 <ahmedoppal2592@gmail.com>
2024-11-21 23:05:22 +0500

    Final Code

---
+++ script.js
@@ -0,0 +1,16 @@
+var x=12;
+
+document.write(x+" Ahmad");
+
+function hello(){
+    var y=9;
+
+    documnet.write("<br>"+x);
+}
+
+// console.log(y);
+
+while(x>=1){
+    documnet.write("<br> Ahmad Uppal");
+    x-=1;
+}
